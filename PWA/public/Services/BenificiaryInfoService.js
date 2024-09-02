cd0e3d4dd89f9f5234749fd7276df1d10302d6c4
rcmenno <129762469+rcmenno@users.noreply.github.com>
2024-09-02 17:21:07 +0200

    Make sure the number of served benificiaries are shown

--- PWA/public/Services/BenificiaryInfoService.js
+++ PWA/public/Services/BenificiaryInfoService.js
@@ -4,10 +4,9 @@ export class BenificiaryInfoService {
     }
     async benificiaryInfoTextFromDistribution(distribution) {
         const benificiaries = await this.database.benificiariesForDistribution(distribution);
-        const numberOfServedBenificiaries = 42; //TODO: implement proper calculation
+        const numberOfServedBenificiaries = benificiaries.filter(benificiary => benificiary.hasBeenMarkedAsReceived).length;
         if (benificiaries.length > 0) {
-            //Beneficiaries served: {{ number_recipients }} / {{ number_beneficiaries }}
-            return "Beneficiaries served: " + numberOfServedBenificiaries;
+            return "Beneficiaries served: " + numberOfServedBenificiaries + " / " + benificiaries.length;
         }
         else {
             return "No beneficiary data found";
